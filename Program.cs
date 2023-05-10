7d68524207b43499878ed3b81814d254441c8d33
ErnestoCisnero <miguelernestocisnero@gmail.com>
2023-05-09 20:30:03 -0400

    C# read and write with TwinCAT3

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TwinCAT.Ads;
+
+namespace TCADStut
+{
+    class Program
+    {
+        public static void Main()
+        {
+            AdsClient myClient = new AdsClient();
+            myClient.Connect(AmsNetId.Local, 851);
+            System.Console.WriteLine("ADS Client has been started.");
+
+            var bCSharpTest = myClient.CreateVariableHandle("MAIN.bCSharpTest");
+
+            System.Console.WriteLine("Before");
+            var resp = myClient.ReadAny(bCSharpTest, typeof(bool));
+            System.Console.WriteLine(resp);
+
+            System.Console.WriteLine("After");
+            myClient.WriteAny(bCSharpTest, false);
+            
+            resp = myClient.ReadAny(bCSharpTest, typeof(bool));
+            System.Console.WriteLine(resp);
+        }
+    }
+}
